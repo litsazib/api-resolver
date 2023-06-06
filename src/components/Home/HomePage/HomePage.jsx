7f2400cf104ac14dad1ec8a83cac7d4bc07e1c74
litsazib <roktimsazib92@gmail.com>
2023-06-06 11:04:53 +0600

    mdofiy

--- src/components/Home/HomePage/HomePage.jsx
+++ src/components/Home/HomePage/HomePage.jsx
@@ -26,33 +26,52 @@ const initialValues = {
 };
 
 // const [loader, setLoader] = useState(true)
+
+const onSubmit = async (values,formikProps) => {
+  let result = await Create(apiLink.hitCreateDentist, values );
+  return result && result.resetForm();
+};
+
 const getDentisDetailsList = async (params)=>{
   let result = await SelectByPageing(apiLink.hitDentistList,params);
   return result && result
 }
+
 const deleteAction = async(item)=>{
   let result = await Delete(apiLink.hitDeleteDentist,item);
   return result && result
 }
+
+// const updateAction = async(type = "update",values,item)=>{
+//   let result = await Update(apiLink.hitUpdateDentist,values,item)
+// }
+
+
+
 const SelectAll = async ()=>{
   let result = await Read(apiLink.hitPostList,defaultMethod);
   return result
 }
+
+
 const SelectOne = async (id)=>{
   let result = await SelectByID(apiLink.hitPostList,id,loader);
   console.log('SelectByID',result)
   return result
 }
+
 const DeleteOne = async (id)=>{
   let result = await Delete(apiLink.hitPostDelete,id,loader);
   console.log('DeleteData',result)
   return result
 }
+
 const validationSchema = Yup.object({
   first_name: Yup.string().required("Required"),
   last_name: Yup.string().required("Required"),
   email: Yup.string().email("Invalid email format").required("Required")
 });
+
 const validateComments = (value) => {
   let error;
   if (!value) {
@@ -70,26 +89,9 @@ function HomePage() {
   const [pagination,setPagination] = useState({})
   const [error, setError] = useState({});
 
-  const onSubmit = async (type="create",values,formikProps) => {
-    if(type === "create") {
-      let result = await Create(apiLink.hitCreateDentist, values );
-      if (result) {
-        formikProps.resetForm();
-      }
-    }else if(type === "update") {
-      let result = await Update(apiLink.hitUpdateDentist, values, updateData);
-      if (result) {
-        formikProps.resetForm();
-        setUpdateData(null);
-        setUpdate(false);
-      }
-    }
-  };
-
-
   const updateAction = (item)=>{
-    setUpdateData(item);
-    setUpdate(true);
+    setUpdateData(item)
+    setUpdate(true)
   }
 
   useEffect(() => {
@@ -112,7 +114,7 @@ function HomePage() {
         <Col md={6} className="mx-auto">
           <h1> <FaBeer/> LIST </h1>
           <ul>
-              {data.slice(0,5).map((item,id)=>{
+              {data?.slice(0,5).map((item,id)=>{
                 return (
                   <li key={item.id}  
                   style={{
@@ -143,11 +145,9 @@ function HomePage() {
         </small>
         <Col md={6} className="mx-auto">
           <Formik
-            initialValues={updateData || initialValues}
+            initialValues={initialValues}
             validationSchema={validationSchema}
-            onSubmit={(values, formikProps) =>
-              onSubmit(update ? "update" : "create", values, formikProps)
-            }
+            onSubmit={onSubmit}
             enableReinitialize
             // validateOnChange={false}
             // validateOnBlur={false}
